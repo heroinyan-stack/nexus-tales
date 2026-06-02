a675271c2b022f21379a6fd91fd9ad6b58ba7a9c
M yan <myan@MdeMacBook-Pro.local>
2026-06-02 17:28:29 +0800

    add: Google Search Console verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="googleb9b9f9e82e3320b9" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
