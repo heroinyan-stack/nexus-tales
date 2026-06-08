37fc53f21fdf8addcd68f6f347a914d23eef31a4
heroinyan-stack <heroinyan-stack@users.noreply.github.com>
2026-06-08 13:10:23 +0800

    feat: add Google AdSense code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="googleb9b9f9e82e3320b9" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8291464992255712" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
